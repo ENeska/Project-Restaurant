41c09d8a0a2478c10a8200f969022c983e235a07
Ela Neska <ela.neska@wp.pl>
2017-03-18 22:26:51 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -128,15 +128,16 @@ $(document).ready(function() {
 });
 
 
-
-var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+    var place = {lat: 38.7223, lng: -9.139329};
+    var place2 = {lat: 38.745637, lng: -8.384338};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: place2
+    });
+    var marker = new google.maps.Marker({
+        position: place,
+        map: map
     });
 }
-
-
-
 
